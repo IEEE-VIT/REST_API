a229ea7bebd03fc515ad726eead9e9b9949c02c4
preethana.su2022@vitstudent.ac.in <preethana.su2022@vitstudent.ac.in>
2023-09-23 17:45:35 +0530

    issue 11

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///library.db'
 # This token needs to be present in the request header to authorize users
 @app.route('/login')
 def login():
+{
     auth=request.authorization
     if not auth or not auth.username or not auth.password:
         return make_response("Could not verify",401,{"WWW-Authenticate" :'Basic realm="Login required"'})
@@ -19,29 +20,29 @@ def login():
         token=jwt.encode({"user_id":user.user_id,"exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=20)},app.config['SECRET_KEY'])
         return jsonify({"token":token})
     return make_response("Could not verify",401,{"WWW-Authenticate" :'Basic realm="Login required"'})
-    def admin():
+}
+        def user():
+    {
         book = input("Enter the name of the book:")
-        print(book.add)
-        print(book.delete)
-        if (details!=0):
-        {
-            book = input("enter the books:")
-            print(user.detail)
-        }
-        else
-        {
-            print("invalid")
-        }
-        
-    def user():
-        if check_password_hash(user.password,auth.password):
-        {
-            print("password is: " + "Isha1237!")
-        }
-        else:
-        {
-            print("Invalid")
-        }
+        if(book_availability_borrowed = True)
+            print(book.update)
+            print("Book updated sucessfully")
+            return book.update
+        elif(book_availability== False)
+            if(book.user_borrowed_id=user_id)
+               update_book_available = input("update book available:")
+               user_borrowed_id = input("The user borroed id is: " + UHGB123)
+               return update_book_available
+               return user_borrowed_id
+            else
+                printf("invalid")
+    elif(book_available_borrowed = False)
+        print("Book available to be borrowed")
+        book_not_available_borrowed = input("Not available")
+        return not_availale
+    else
+        print("Invalid")
+    }
 
 
 
